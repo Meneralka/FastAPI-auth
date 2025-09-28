98483e56ea3e96741075603fef1f4c331253e26c
mener <frutash32@gmail.com>
2025-09-28 11:49:35 +0300

    access token has been around for too long

--- auth/core/config.py
+++ auth/core/config.py
@@ -33,7 +33,7 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
-    access_token_expire_minutes: int = 180
+    access_token_expire_minutes: int = 10
     refresh_token_expire_days: int = 30
 
 
