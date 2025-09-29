fce10adad8a0bd7b11cb0c0858f239640054c2a0
mener <frutash32@gmail.com>
2025-09-29 22:27:54 +0300

    remove useless functions

--- auth/api/api_v1/__init__.py
+++ auth/api/api_v1/__init__.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter
 
 from core.config import settings
 
-from .profiles.users import router as users_router
-from .authentication.jwt_auth import router as demo_auth_router
+from .authentication.jwt_auth import router as auth_router
 from .authentication.register import router as register_router
 
 router = APIRouter(
@@ -11,11 +10,7 @@ router = APIRouter(
 )
 
 router.include_router(
-    users_router,
-    prefix=settings.api.v1.users,
-)
-router.include_router(
-    demo_auth_router,
+    auth_router,
 )
 
 router.include_router(
