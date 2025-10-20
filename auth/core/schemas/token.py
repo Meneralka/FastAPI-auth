13e6fd0d296502e4db31153f6d64a3b993ef4239
Mineralkin <frutash32@gmail.com>
2025-10-20 23:58:43 +0300

    small refactor

--- auth/core/schemas/token.py
+++ auth/core/schemas/token.py
@@ -1,13 +1,20 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, Field, field_validator
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    field_validator,
+)
 from core.models.user import SessionStatus
+
 
 class Tokens(BaseModel):
     access_token: str
     refresh_token: str | None = None
     token_info: str = "Bearer"
+
 
 class TokenBase(BaseModel):
     model_config = ConfigDict(
@@ -19,12 +26,15 @@ class TokenBase(BaseModel):
     status: SessionStatus = SessionStatus.ACTIVE
     sub: str
 
+
 class TokenInfo(TokenBase):
     id: int
     timestamp: datetime
 
+
 class TokenCreate(TokenBase):
     pass
+
 
 class SessionBase(BaseModel):
     model_config = ConfigDict(
@@ -36,11 +46,13 @@ class SessionBase(BaseModel):
     sub: str
     ip: str
     status: Optional[SessionStatus] = Field(
-            default= SessionStatus.ACTIVE, validate_default=True
-        )
+        default=SessionStatus.ACTIVE, validate_default=True
+    )
+
 
 class SessionCreate(SessionBase):
     status: SessionStatus = SessionStatus.ACTIVE
+
 
 class SessionRead(SessionBase):
     id: str
