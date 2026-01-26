369ad1b20d93f48039ed907e424896a3d43802d1
mener <frutash32@gmail.com>
2026-01-27 00:08:19 +0300

    delete useless construction

--- auth/main.py
+++ auth/main.py
@@ -29,8 +29,3 @@ register_exception_handlers(main_app)
 main_app.include_router(
     api_router,
 )
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
-    )
