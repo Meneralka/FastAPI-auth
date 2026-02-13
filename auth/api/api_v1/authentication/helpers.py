2765ac313cdb5425207f0d7404f74a8e42dc2f0b
Mineralkin <frutash32@gmail.com>
2026-02-13 13:24:54 +0300

    move helpers

--- auth/api/api_v1/authentication/helpers.py
+++ auth/api/api_v1/authentication/helpers.py
@@ -1,11 +1,17 @@
 from datetime import timedelta
-from typing import Annotated
+from typing import Annotated, Any
+
+import jwt
+from jwt import PyJWKClient
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Form, Depends, Request
 from loguru import logger as log
+from starlette.requests import Request
+from starlette.responses import Response
+from user_agents import parse
 
-from api.exceptions.auth import InvalidCredentialsException
+from api.exceptions.auth import InvalidCredentialsException, NoIdTokenException
 
 from core.config import (
     settings,
@@ -14,7 +20,7 @@ from core.config import (
     REFRESH_TOKEN_TYPE,
 )
 from core.models import User, db_helper
-from core.schemas.user import UserAuth, UserReg
+from core.schemas.user import UserAuth, UserReg, UserRead
 from crud.users import get_user_by_username, create_user
 from utils import auth as auth_utils
 
@@ -77,3 +83,75 @@ async def validate_google_id(
 
 create_access_token = CreateToken(ACCESS_TOKEN_TYPE)
 create_refresh_token = CreateToken(REFRESH_TOKEN_TYPE)
+
+
+async def create_and_set_tokens_to_cookie(
+    response: Response, user: UserRead, got_session: Any
+):
+    access_token = await create_access_token(
+        user=user,
+        session_uuid=got_session.uuid,
+    )
+    refresh_token = await create_refresh_token(
+        user=user,
+        session_uuid=got_session.uuid,
+    )
+    response.set_cookie(
+        "refresh_token",
+        refresh_token,
+        max_age=settings.auth.refresh_token_expire_days * 24 * 60 * 60,
+        httponly=True,
+        secure=True,
+    )
+
+    response.set_cookie(
+        "access_token",
+        access_token,
+        max_age=settings.auth.access_token_expire_minutes * 60,
+        httponly=True,
+        secure=True,
+    )
+    return response
+
+
+async def get_json_google_token(code: str):
+    data = {
+        "code": code,
+        "client_id": settings.google.client_id,
+        "client_secret": settings.google.client_secret,
+        "redirect_uri": settings.google.redirect_uri,
+        "grant_type": settings.google.grant_type,
+    }
+
+    async with httpx.AsyncClient() as client:
+        token_response = await client.post(settings.google.token_url, data=data)
+        token_json = token_response.json()
+
+        return token_json
+
+
+def get_google_user_payload(token_json: Any):
+    id_token = token_json.get("id_token")
+    if not id_token:
+        raise NoIdTokenException
+
+    jwk_client = PyJWKClient(settings.google.jwks_url)
+    signing_key = jwk_client.get_signing_key_from_jwt(id_token)
+    payload = jwt.decode(
+        id_token,
+        signing_key.key,
+        algorithms=[settings.google.algorithm],
+        audience=settings.google.client_id,
+        issuer=["https://accounts.google.com", "accounts.google.com"],
+    )
+
+    return payload
+
+
+def get_session_name(request: Request) -> str:
+    user_agent_str = parse(request.headers.get("User-Agent", "Unknown"))
+    session_name = (
+        f"{user_agent_str.browser.family} {user_agent_str.browser.version_string},"
+        f" {user_agent_str.os.family} {user_agent_str.os.version_string}"
+    )
+    return session_name
